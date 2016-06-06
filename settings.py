3d037051935cf5895dde5a2ee6d1d70e2364e824
aga <agne6ka@gmail.com>
2016-06-06 18:55:06 +0200

    Add avatar scrapping after user signs up. Update settings, fix allauth errors. Fix broken django admin url.

--- settings.py
+++ settings.py
@@ -24,6 +24,7 @@ THIRD_PARTY_APPS = (
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
+    'django_extensions',  # usefull stuff, ex. shell_plus
 )
 
 BUDDY_APPS = (
@@ -90,7 +91,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.User'
 
 ACCOUNT_ADAPTER = 'users.adapters.AccountAdapter'
@@ -103,7 +103,7 @@ SOCIALACCOUNT_PROVIDERS = {
     'facebook': {
         'METHOD': 'js_sdk',  # 'oauth2',
         'SCOPE': ['email', 'public_profile', 'user_friends'],
-        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        # 'AUTH_PARAMS': {'auth_type': 'reauthenticate'},  # Should we always ask for password?
         'FIELDS': [
             'id',
             'email',
@@ -126,3 +126,13 @@ SOCIALACCOUNT_PROVIDERS = {
         'METHOD': 'oauth',
     },
 }
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
