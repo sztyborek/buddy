773ca53a27d5a864f84ca9b982e768842ebf6c0c
kasia <na.slupeckiej@gmail.com>
2016-06-15 16:33:42 -0400

    Add a skill subform to ProjectUpdateView
    In ProjectUpdateView you can add skills to a project. In case an added
    skill doesn't exist in the database it is also added to the database.

--- projects/forms.py
+++ projects/forms.py
@@ -1,36 +1,33 @@
 from django import forms
 from django.forms import ModelForm, Textarea, TextInput, NumberInput
 from django.forms.extras import SelectDateWidget
+from django.forms import modelformset_factory
 from .models import Project
 from users.models import Skill
 
-class SkillsTextInput(TextInput):
-    def _format_value(self, value, *args, **kwargs):
-        print value
-        for skill in value:
-            print skill.programming_lang
-        super(SkillsTextInput, self)._format_value(value, *args, **kwargs)
-
-print dir(SkillsTextInput)
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('name', 'description', 'expiration_date', 'number_of_users_required', 'opensource', 'url', 'skills')
+        fields = ('name', 'description', 'expiration_date', 'number_of_users_required', 'opensource', 'url')
         widgets = {
 	    'name': Textarea(attrs={'rows':2, 'cols':50}),
 	    'description': Textarea(attrs={'rows':2, 'cols':45}),
 	    'expiration_date': SelectDateWidget(),
 	    'number_of_users_required': NumberInput(attrs={'style':'text-align:right'}),
 	    'url': TextInput(attrs={'size':54}),
-        'skills': SkillsTextInput(),
 	    }
 
 class SkillForm(ModelForm):
-    programming_lang = forms.CharField(required=False)
     class Meta:
         model = Skill
-        exclude = ('skills',)
+        fields = ('programming_lang',)
+
+    def __init__(self, *args, **kwargs):
+        super(SkillForm, self).__init__(*args, **kwargs)
+        self.fields['programming_lang'].required = False
 
     def validate_unique(self):
         return True
+
+SkillFormSet = modelformset_factory(Skill, form=SkillForm, fields=('programming_lang',), extra=3)
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Project
-from .forms import ProjectForm
+from users.models import Skill
+from .forms import ProjectForm, SkillFormSet
+from django.http import HttpResponseRedirect
 
 
 class ProjectListView(ListView):
@@ -19,28 +21,30 @@ class ProjectCreateView(CreateView):
 class ProjectUpdateView(UpdateView):
     model = Project
     form_class = ProjectForm
-    subform_class = SkillForm
+    subform_class = SkillFormSet
 
     def post(self, request, *args, **kwargs):
-            self.object = self.get_object()
-            form = self.form_class(request.POST, instance=self.object)
-            subform = self.subform_class(request.POST)
-            if form.is_valid() and subform.is_valid():
-                print subform.cleaned_data
-                this_project = form.save(commit=False)
-                a_lang = subform.data['programming_lang'].lower()
+        self.object = self.get_object()
+        form = self.form_class(request.POST, instance=self.object)
+        subform = self.subform_class(request.POST)
+        if form.is_valid() and subform.is_valid():
+            this_project = form.save(commit=False)
+            this_project.skills.clear()
+            for a_form in subform:
+                a_lang = a_form.cleaned_data.get('programming_lang')
                 if a_lang:
+                    a_lang = a_lang.lower()
                     skill, created = Skill.objects.get_or_create(programming_lang=a_lang)
                     this_project.skills.add(skill)
-                this_project.save()
-                return HttpResponseRedirect(self.get_success_url())
-            else:
-                return self.render_to_response(
-                    self.get_context_data(form=form, subform=subform))
+            this_project.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.render_to_response(
+                self.get_context_data(form=form, subform=subform))
 
     def get_context_data(self, **kwargs):
         context = super(ProjectUpdateView, self).get_context_data(**kwargs)
-        context['subform'] = self.subform_class(self.request.GET)
+        context['subform'] = self.subform_class(queryset = self.get_object().skills.all())
         return context
 
     def get_success_url(self):
