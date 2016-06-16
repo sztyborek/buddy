5371eb00be86695d74c8ceb7721256a32e7e6f0b
kasia <na.slupeckiej@gmail.com>
2016-06-16 09:45:04 -0400

    Add a skill subform to ProjectCreateView
    In ProjectCreateView you can add skills to a project
    the same way as in ProjectUpdateView.

--- projects/views.py
+++ projects/views.py
@@ -15,6 +15,36 @@ class ProjectDetailView(DetailView):
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectForm
+    subform_class = SkillFormSet
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.form_class(request.POST)
+        subform = self.subform_class(request.POST)
+        if form.is_valid() and subform.is_valid():
+            this_project = form.save(commit=False)
+            this_project.save()
+            for a_form in subform:
+                a_lang = a_form.cleaned_data.get('programming_lang')
+                if a_lang:
+                    a_lang = a_lang.lower()
+                    skill, created = Skill.objects.get_or_create(programming_lang=a_lang)
+                    this_project.skills.add(skill)
+            this_project.save()
+            self.object = Project.objects.get(slug=this_project.slug)
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.render_to_response(
+                self.get_context_data(form=form, subform=subform))
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectCreateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['subform'] = self.subform_class(self.request.POST)
+        else:
+            context['subform'] = self.subform_class(queryset = Skill.objects.none())
+        return context
+        
     def get_success_url(self):
         return reverse_lazy('projects:detail', args=(self.object.slug,)) 
 
